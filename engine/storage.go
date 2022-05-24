d541940e29c2263739af8f04739993cd2eb7c491
nireo <eemil.lehtikangas1@gmail.com>
2022-05-24 16:01:19 +0300

    added a simple storage engine using a pre-existing key-value database to ease development

---
+++ engine/storage.go
@@ -0,0 +1,50 @@
+package engine
+
+import (
+	badger "github.com/dgraph-io/badger/v3"
+)
+
+type Storage interface {
+	Put([]byte, []byte) error
+	Get([]byte) ([]byte, error)
+	Delete([]byte) error
+}
+
+// implements the storage
+type KVStore struct {
+	db *badger.DB
+}
+
+func (kv *KVStore) Put(key, value []byte) error {
+	return kv.db.Update(func(txn *badger.Txn) error {
+		return txn.Set(key, value)
+	})
+}
+
+func (kv *KVStore) Get(key []byte) ([]byte, error) {
+	var valCopy []byte
+	err := kv.db.View(func(txn *badger.Txn) error {
+		item, err := txn.Get(key)
+		if err != nil {
+			return err
+		}
+
+		valCopy, err = item.ValueCopy(nil)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return valCopy, nil
+}
+
+func (kv *KVStore) Delete(key []byte) error {
+	return kv.db.Update(func(txn *badger.Txn) error {
+		return txn.Delete(key)
+	})
+}
