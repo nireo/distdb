116080fd0ebb90e83a9ccf5ee505a104261d365e
nireo <eemil.lehtikangas1@gmail.com>
2022-05-24 23:18:43 +0300

    added protobuf and some proto definitions

--- engine/storage.go
+++ engine/storage.go
@@ -4,10 +4,17 @@ import (
 	badger "github.com/dgraph-io/badger/v3"
 )
 
+// Common storage interface interact with internal storage of a
+// distributed node.
 type Storage interface {
 	Put([]byte, []byte) error
 	Get([]byte) ([]byte, error)
 	Delete([]byte) error
+}
+
+type KVPair struct {
+	Key   []byte
+	Value []byte
 }
 
 // implements the storage
@@ -15,12 +22,15 @@ type KVStore struct {
 	db *badger.DB
 }
 
+// Put places a key into the database.
 func (kv *KVStore) Put(key, value []byte) error {
 	return kv.db.Update(func(txn *badger.Txn) error {
 		return txn.Set(key, value)
 	})
 }
 
+// Get finds a given key from the database or returns a error, if that
+// key was not found.
 func (kv *KVStore) Get(key []byte) ([]byte, error) {
 	var valCopy []byte
 	err := kv.db.View(func(txn *badger.Txn) error {
@@ -43,12 +53,14 @@ func (kv *KVStore) Get(key []byte) ([]byte, error) {
 	return valCopy, nil
 }
 
+// Delete removes a given key from the database.
 func (kv *KVStore) Delete(key []byte) error {
 	return kv.db.Update(func(txn *badger.Txn) error {
 		return txn.Delete(key)
 	})
 }
 
+// NewKVStore creates a badger.DB instance with generally good settings.
 func NewKVStore() (*KVStore, error) {
 	db, err := badger.Open(badger.DefaultOptions("./"))
 	if err != nil {
@@ -57,6 +69,8 @@ func NewKVStore() (*KVStore, error) {
 	return &KVStore{db: db}, nil
 }
 
+// NewKVStoreWithPath creates a badge.DB instance with the same options as
+// NewKVStore, but it also includes a given path.
 func NewKVStoreWithPath(path string) (*KVStore, error) {
 	db, err := badger.Open(badger.DefaultOptions(path))
 	if err != nil {
@@ -65,6 +79,9 @@ func NewKVStoreWithPath(path string) (*KVStore, error) {
 	return &KVStore{db: db}, nil
 }
 
+// Close closes the connection to the badger database. Since we don't
+// want to defer closing in the creation function, we need to create a
+// custom close method here.
 func (kv *KVStore) Close() {
 	kv.db.Close()
 }
--- go.mod
+++ go.mod
@@ -12,12 +12,19 @@ require (
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b // indirect
 	github.com/golang/groupcache v0.0.0-20190702054246-869f871628b6 // indirect
-	github.com/golang/protobuf v1.3.1 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/golang/snappy v0.0.3 // indirect
 	github.com/google/flatbuffers v1.12.1 // indirect
+	github.com/google/go-cmp v0.5.6 // indirect
 	github.com/klauspost/compress v1.12.3 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	go.opencensus.io v0.22.5 // indirect
-	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
-	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
+	golang.org/x/net v0.0.0-20220520000938-2e3eb7b945c2 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	google.golang.org/genproto v0.0.0-20220524164028-0aa58a9287dd // indirect
+	google.golang.org/grpc v1.46.2 // indirect
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.2.0 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
 )
