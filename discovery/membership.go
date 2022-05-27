8fec6a19183fced0b8e6753c01114fbf561baf7c
nireo <eemil.lehtikangas1@gmail.com>
2022-05-27 14:16:03 +0300

    finished the simple discovery module

--- discovery/membership.go
+++ discovery/membership.go
@@ -69,3 +69,57 @@ func (m *Membership) setupSerf() (err error) {
 
 	return nil
 }
+
+func (m *Membership) eventHandler() {
+	for e := range m.events {
+		switch e.EventType() {
+		case serf.EventMemberJoin:
+			for _, member := range e.(serf.MemberEvent).Members {
+				if m.isLocal(member) {
+					continue
+				}
+				m.handleJoin(member)
+			}
+		case serf.EventMemberLeave, serf.EventMemberFailed:
+			for _, member := range e.(serf.MemberEvent).Members {
+				if m.isLocal(member) {
+					return
+				}
+				m.handleLeave(member)
+			}
+		}
+	}
+}
+
+func (m *Membership) handleJoin(member serf.Member) {
+	if err := m.handler.Join(member.Name, member.Tags["rpc_addr"]); err != nil {
+		m.logError(err, "failed to join", member)
+	}
+}
+
+func (m *Membership) handleLeave(member serf.Member) {
+	if err := m.handler.Leave(member.Name); err != nil {
+		m.logError(err, "failed to leave", member)
+	}
+}
+
+func (m *Membership) isLocal(member serf.Member) bool {
+	return m.serf.LocalMember().Name == member.Name
+}
+
+func (m *Membership) Members() []serf.Member {
+	return m.serf.Members()
+}
+
+func (m *Membership) Leave() error {
+	return m.serf.Leave()
+}
+
+func (m *Membership) logError(err error, msg string, member serf.Member) {
+	m.logger.Error(
+		msg,
+		zap.Error(err),
+		zap.String("name", member.Name),
+		zap.String("rcp_addr", member.Tags["rpc_addr"]),
+	)
+}
