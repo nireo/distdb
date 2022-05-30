81a6d8a13553f1ca555b3d7f4e87ed7d1c494e7c
nireo <eemil.lehtikangas1@gmail.com>
2022-05-30 23:51:06 +0300

    added raft streamlayer

--- engine/distributed.go
+++ engine/distributed.go
@@ -3,10 +3,12 @@ package engine
 import (
 	"bytes"
 	"crypto/tls"
+	"fmt"
 	"io"
 	"net"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/hashicorp/raft"
 	api "github.com/nireo/distdb/api/v1"
@@ -37,6 +39,67 @@ type StreamLayer struct {
 	ln              net.Listener
 	serverTLSConfig *tls.Config
 	peerTLSConfig   *tls.Config
+}
+
+const RaftRPC = 1
+
+func NewStreamLayer(ln net.Listener, serverTLSConfig,
+	peerTLSConfig *tls.Config) *StreamLayer {
+	return &StreamLayer{
+		ln:              ln,
+		serverTLSConfig: serverTLSConfig,
+		peerTLSConfig:   peerTLSConfig,
+	}
+}
+
+func (s *StreamLayer) Dial(addr raft.ServerAddress, timeout time.Duration) (
+	net.Conn, error,
+) {
+	dialer := &net.Dialer{Timeout: timeout}
+	var conn, err = dialer.Dial("tcp", string(addr))
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = conn.Write([]byte{byte(RaftRPC)})
+	if err != nil {
+		return nil, err
+	}
+
+	if s.peerTLSConfig != nil {
+		conn = tls.Client(conn, s.peerTLSConfig)
+	}
+	return conn, err
+}
+
+func (s *StreamLayer) Accept() (net.Conn, error) {
+	conn, err := s.ln.Accept()
+	if err != nil {
+		return nil, err
+	}
+
+	b := make([]byte, 1)
+	if _, err := conn.Read(b); err != nil {
+		return nil, err
+	}
+
+	if bytes.Compare([]byte{(byte(RaftRPC))}, b) != 0 {
+		return nil, fmt.Errorf("not a raft rpc")
+	}
+
+	if s.serverTLSConfig != nil {
+		return tls.Server(conn, s.serverTLSConfig), nil
+	}
+
+	return conn, nil
+}
+
+func (s *StreamLayer) Close() error {
+	return s.ln.Close()
+}
+
+func (s *StreamLayer) Addr() net.Addr {
+	return s.ln.Addr()
 }
 
 type fsm struct {
